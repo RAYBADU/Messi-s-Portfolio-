4beeb6d7b94042111a701050a934d523fae06c76
RAYBADU <raymondafrani00@gmail.com>
2025-09-12 20:23:36 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const menu = document.getElementById("menu")
+const hamBtn = document.getElementById("ham-btn")
+const closeBtn = document.getElementById("close-btn")
+const body = document.getElementById("body")
+
+
+hamBtn.addEventListener("click", ()=>{
+    menu.classList.toggle("-translate-x-full")
+})
+
+closeBtn.addEventListener("click", ()=>{
+        menu.classList.toggle("-translate-x-full")
+
+})
+
+
+setTimeout(()=>{
+    
+})
